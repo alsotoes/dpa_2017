ff9dd6d71b3c39c16c0ccf9c97bec38c9c4a190c
Alvaro Soto <alvaro@skyline.local>
2017-02-01 20:27:24 -0600

    Downloads, count and disk usage in one line

--- hwk1.sh
+++ hwk1.sh
@@ -1,7 +1,5 @@
 # This downloads all files needed
-seq -f "%02g" 1 31 | parallel -j31 wget http://data.gdeltproject.org/events/201612{}.export.CSV.zip
-du -h .  
-ls *.zip | wc -l
+seq -f "%02g" 1 31 | parallel -j31 wget http://data.gdeltproject.org/events/201612{}.export.CSV.zip &>/dev/null ; ls -l *.zip | wc -l ; du -h .
 
 #zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | sed 's/\t+/ \t/p' | csvsql --db sqlite:///gdelt.db --insert --table mexico -t
 #zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | head -1 | sed "s/[[:space:]]\+/\t/g"
