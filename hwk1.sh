f6e855dfb4d475ef2aaf791a577cec16b85376c6
Alvaro Soto <alvaro@skyline.headup.local>
2017-02-07 01:01:24 -0600

    Part 1: This downloads all needed files Part 2: Processing the downloaded files puttinh the right headers in database Part 3: ...

--- hwk1.sh
+++ hwk1.sh
@@ -1,7 +1,11 @@
-# This downloads all files needed
+#!/bin/sh
+
+#   This downloads all needed files, 
+#   CMD: Downloads, count and disk usage in one line
 seq -f "%02g" 1 31 | parallel -j31 wget http://data.gdeltproject.org/events/201612{}.export.CSV.zip &>/dev/null ; ls -l *.zip | wc -l ; du -h .
 
-#zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | sed 's/\t+/ \t/p' | csvsql --db sqlite:///gdelt.db --insert --table mexico -t
-#zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | head -1 | sed "s/[[:space:]]\+/\t/g"
-
-zcat 20161201.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | sed "s/\t\+\t/g" | csvsql --db sqlite:///gdelt.db --insert --tables mexico -t -H
+#   Processing the downloaded files puttinh the right headers in database
+#   CMD: Re-creates file names, in parallel unzip all files and search in Actor1CountryCode for /MEX/ and in EventCode /MX/
+seq -f "%02g" 1 31 | parallel zcat 201612{}.export.CSV.zip | awk '( $8~/MEX/ || $27~/MX/)' | sed "s/[[:space:]]\+/\t/g" | \
+sed '1s/^/'"$(wget -nv -qO- http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt | xargs -n1 | awk 'NR%58{printf "%s\t",$0;next}7')"'\n/gm' | \
+csvsql --db sqlite:///mexico.db --insert --tables mexico -t
