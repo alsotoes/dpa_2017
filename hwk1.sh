7c565f07affe0ee25eb65b75a6417a47d9fb429b
mvilchis <mvilchis@localhost.localdomain>
2017-02-07 02:07:45 -0600

    Goldein

--- hwk1.sh
+++ hwk1.sh
@@ -1,7 +1,21 @@
-# This downloads all files needed
-seq -f "%02g" 1 31 | parallel -j31 wget http://data.gdeltproject.org/events/201612{}.export.CSV.zip &>/dev/null ; ls -l *.zip | wc -l ; du -h .
+#!/bin/sh
 
-#zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | sed 's/\t+/ \t/p' | csvsql --db sqlite:///gdelt.db --insert --table mexico -t
-#zcat 20170128.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | head -1 | sed "s/[[:space:]]\+/\t/g"
+#   This downloads all needed files, 
+#   CMD: Downloads, count and disk usage in one line
+#seq -f "%02g" 1 31 | parallel wget http://data.gdeltproject.org/events/201612{}.export.CSV.zip &>/dev/null ; ls -l *.zip | wc -l ; du -h .
 
-zcat 20161201.export.CSV.zip | awk '{if ($27 ~ /MX/) print}' | sed "s/\t\+\t/g" | csvsql --db sqlite:///gdelt.db --insert --tables mexico -t -H
+#   Processing the downloaded files putting the right headers in database
+#   CMD: Re-creates file names, in parallel unzip all files and search in Actor1CountryCode for /MEX/ and in EventCode for /MX/
+#seq -f "%02g" 1 31 | parallel zcat 201612{}.export.CSV.zip | awk '( $8~/MEX/ || $27~/MX/)' | sed "s/[[:space:]]\+/\t/g" | \
+#sed '1s/^/'"$(wget -nv -qO- http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt | xargs -n1 | awk 'NR%58{printf "%s\t",$0;next}7')"'\n/gm' | \
+#csvsql --db sqlite:///mexico.db --insert --tables mexico -t
+
+
+seq -f "%02g" 1 31 | parallel zcat 201612{}.export.CSV.zip | awk '( $8~/MEX/ || $27~/MX/)' |\
+sed '1s/^/'"$(wget -nv -qO- http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt |\
+xargs -n1 | awk 'NR%58{printf "%s\t",$0;next}7')"'\n/gm' | seq -f "%02g" 1 31 | parallel zcat 201612{}.export.CSV.zip |\
+awk '( $8~/MEX/ || $27~/MX/)' | sed '1s/^/'"$(wget -nv -qO- http://gdeltproject.org/data/lookups/CSV.header.dailyupdates.txt |\
+xargs -n1 | awk 'NR%58{printf "%s\t",$0;next}7')"'\n/gm' |\
+tee >(awk -F $'\t' '{ a[$57]++; b[$57]=$31} END { for (i in a) print i, ","a[i], ","b[i]| "sort -nk 1"}'| csvsql --db sqlite:///mexico.db --insert --tables mexico_ts -t ) |\
+csvsql --db sqlite:///mexico.db --insert --tables mexico -t 
+
